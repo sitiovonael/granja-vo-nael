acab72968c44fb40e360b31bda5030bb87f41349
sitiovonael <sitiovonael@gmail.com>
2026-04-14 09:58:12 -0300

    Update AuthContext.jsx

--- granja-vo-nael/src/contexts/AuthContext.jsx
+++ granja-vo-nael/src/contexts/AuthContext.jsx
@@ -66,7 +66,7 @@ export function AuthProvider({ children }) {
 
   async function sendPasswordReset(email) {
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: window.location.origin,
+      redirectTo: window.location.origin + '/redefinir-senha',
     })
     return { error }
   }
